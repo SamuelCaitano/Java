9c5a90fa6d24ec51e6700a4c8c1c22c0fb25f6fa
Samuel Caitano <smlcaitano@gmail.com>
2021-08-17 23:09:43 -0300

    Account commit

---
+++ Account/AccounTest.java
@@ -0,0 +1,21 @@
+package appe;
+
+/**
+ *
+ * @author samuelCaitano (ref. JavaHowToProgram - Paul Deitel)
+ */
+public class AccounTest {
+
+    public static void main(String[] args) {
+        Account account = new Account("Samuel Caitano", 75.00);
+
+        System.out.printf("Saldo inicial da conta: $%.2f ", account.getBalance());
+
+        double depositAmount = 25.0;
+
+        System.out.printf("\nAdicionando $%.2f ao saldo da conta\n\n", depositAmount);
+        account.deposit(depositAmount);
+
+        System.out.printf("Novo saldo da conta: $%.2f ", account.getBalance());
+    }
+}
