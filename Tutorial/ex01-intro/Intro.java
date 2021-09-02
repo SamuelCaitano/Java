20a3656e078cd5717daa95b25f2884b6bf593184
Samuel Caitano <smlcaitano@gmail.com>
2021-09-02 11:43:57 -0300

    new commit

--- Tutorial/ex01-intro/Intro.java
+++
@@ -1,8 +0,0 @@
-package intro;
-
-public class Intro {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }    
-}
---
+++ Tutorial/ex01_intro/Intro.java
@@ -0,0 +1,8 @@
+public class Intro 
+{
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    } // fim do método main    
+} // fim da classe Intro
