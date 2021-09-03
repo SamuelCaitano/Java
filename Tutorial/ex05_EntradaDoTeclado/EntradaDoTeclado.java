413438582f81cd1ecdb02d28257e5d4645d334f0
Samuel Caitano <smlcaitano@gmail.com>
2021-09-02 23:11:35 -0300

    new commit
    Entrada de dados pelo usuário (Scanner)

---
+++ Tutorial/ex05_EntradaDoTeclado/EntradaDoTeclado.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; // programa utiliza a classe Scanner para a entrada de dados
+
+public class EntradaDoTeclado 
+{
+    // método main inicia a execusão do aplicativo Java
+    public static void main(String[] args) 
+    {
+        // cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+        
+        int num1;
+        int num2;
+        int soma;
+        
+        System.out.print("Entre com o primeiro número inteiro: ");
+        num1 = input.nextInt(); // lê o primeiro número fornecido pelo usúario
+        
+        System.out.print("Entre com o segundo número inteiro: ");
+        num2 = input.nextInt(); // lê o segundo número fornecido pelo usúario
+        
+        soma = num1 + num2; // soma os números e armazena o total em soma
+        
+        System.out.printf("A soma de %d + %d = %d%n", num1, num2, soma);
+    }
+}
