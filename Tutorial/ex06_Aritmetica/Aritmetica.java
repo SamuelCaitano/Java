d9fe18f28855f687de13df3fcb31c059a6397cfe
Samuel Caitano <smlcaitano@gmail.com>
2021-09-03 00:35:21 -0300

    new commit
    Calculos aritméticos

---
+++ Tutorial/ex06_Aritmetica/Aritmetica.java
@@ -0,0 +1,19 @@
+package aritmetica;
+
+public class Aritmetica 
+{
+    // método main inicia a execusão do aplicativo Java
+    public static void main(String[] args) 
+    {        
+        // váriaveis do tipo int
+        int a = 10; 
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int soma;
+
+        soma = a * b / c + (d - e); // efetua o calculo aritmético
+        System.out.println(soma);
+    } // fim do método main 
+} // fim da classe Aritmetica
