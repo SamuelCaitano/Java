b800659762c830dca2e52edccdb528e55e28a5a6
Samuel Caitano <smlcaitano@gmail.com>
2021-09-09 23:38:24 -0300

    new commit
    Códigos e documentos de diagramas da UML

---
+++ Tutorial/ex13_Do_While/DoWhile.java
@@ -0,0 +1,17 @@
+// instrução de repétição do...while.
+
+public class DoWhile 
+{
+    public static void main(String[] args) 
+    {        
+        int cont = 1;
+                
+        do 
+        {
+            System.out.println(cont + "º execussão");
+            cont++;
+        } while (cont <= 10);
+    }    
+}
+
+
